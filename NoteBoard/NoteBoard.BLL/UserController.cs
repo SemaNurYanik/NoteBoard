85aa726488f590000f09c23c5a55b131d77abf21
SemaNurYanik <nursemayanik@gmail.com>
2019-11-18 14:46:53 +0300

    Second_Commit

--- NoteBoard/NoteBoard.BLL/UserController.cs
+++ NoteBoard/NoteBoard.BLL/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using NoteBoard.DAL.Repositories;
+using NoteBoard.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,5 +16,12 @@ namespace NoteBoard.BLL
             _userDAL = new UserDAL();
 
         }
+
+        public bool Add(User user)
+        {
+            //Kullanıcı kayıt olduktan sonra kullanıcı hesabı admin onayından geçmeden kullanıcı giriş yapamasın 
+        }
+
+
     }
 }
